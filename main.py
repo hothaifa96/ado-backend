d4baeb807eaf3fea15bee7eee33fdd34e642baa2
hothaifa zoubi <hothaifazoubi@gmail.com>
2026-01-09 12:38:32 +0200

    emp stats added

--- main.py
+++ main.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
     return "Hello from the ADO Flask app!"
 
+@app.route("/hello")
+def hello():
+    return "Hegadnjlbnad;kv adlknvda lgap!"
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
